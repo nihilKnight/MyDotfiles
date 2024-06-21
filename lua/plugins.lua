32c79417202f4f5d55eed18b7ca643f5ca381a7a
gentleGuardian123 <takigawa_yoshino@163.com>
2024-06-21 17:37:39 +0800

    updated nvim config.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,6 +45,15 @@ require("lazy").setup({
 		version = "v2.*",
 	},
 
-  
-  "tanvirtin/monokai.nvim",
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
+		},
+		config = function()
+			require("config.nvim-tree")
+		end,
+	},
+
 })
